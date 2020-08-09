814a50ece29ebdcd280f0bfcc3db984d2d8805e7
unknown <klasyer@gmail.com>
2020-08-09 20:53:49 +0300

    git commit --all

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app) 
 ma = Marshmallow(app)
 
+#Classs for the todo list 
 class Todo(db.Model): 
     id = db.Column(db.Integer, primary_key =True) 
     content = db.Column(db.String(200),nullable=False) 
@@ -28,6 +29,7 @@ class TaskSchema(ma.Schema):
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
 
+#Adding a task through the form 
 @app.route('/',methods=['POST','GET']) 
 def index(): 
     if request.method == 'POST': 
@@ -45,6 +47,7 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html',tasks = tasks)  
 
+#Deleting a task though the form 
 @app.route('/delete/<int:id>', methods=['POST','GET']) 
 def delete(id): 
     task_to_delete = Todo.query.get_or_404(id) 
@@ -56,6 +59,7 @@ def delete(id):
     except: 
         return 'Couldnt delete that task, rip'
 
+#Updating a task through the form 
 @app.route('/update/<int:id>', methods=['POST','GET'])
 def update(id): 
     task = Todo.query.get_or_404(id) 
@@ -71,25 +75,60 @@ def update(id):
     else: 
         return render_template('update.html',task=task)
 
+#Post request for adding a task
 @app.route('/task', methods=['POST'])
 def add_task():
-  content = request.json['content']
+    content = request.json['content']
+    new_task = Todo(content)
+    try:
+        db.session.add(new_task)
+        db.session.commit()
+        return task_schema.jsonify(new_task) 
+    except: 
+        return new_task
 
-  new_task = Todo(content)
-
-  db.session.add(new_task)
-  db.session.commit()
-
-  return task_schema.jsonify(new_task) 
-
+#Get request for one task 
 @app.route('/task/<int:id>', methods=['GET'])
 def get_task(id):
     task = Todo.query.get(id)
-   # if task is 
     try:
         return task_schema.jsonify(task)
     except:
         return task
+
+#Get request for all tasks 
+@app.route('/task', methods=['GET'])
+def get_products():
+    all_tasks = Todo.query.all()
+    result = tasks_schema.dump(all_tasks)
+    try: 
+        return tasks_schema.jsonify(result)
+    except: 
+        return 'Unforunate, but there was a problem with getting all the data, most unfortunate...'
+
+#Delete request for deleting a task 
+@app.route('/task/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task_to_delete = Todo.query.get(id)
+    try: 
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return task_schema.jsonify(task_to_delete) 
+    except: 
+        return 'Deleting failed QQ'
+
+#Put request for updating a task 
+@app.route('/task/<int:id>', methods=['PUT'])
+def update_task(id):
+    task_to_update = Todo.query.get(id)
+    content = request.json['content']
+
+    task_to_update.content = content
+    try: 
+        db.session.commit()
+        return task_schema.jsonify(task_to_update) 
+    except: 
+        return task_to_update 
 
 if __name__ == "__main__": 
     app.run(debug=True)
